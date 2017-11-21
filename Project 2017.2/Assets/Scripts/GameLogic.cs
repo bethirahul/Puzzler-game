6b0c2ccbaec62f1268d38d81374e6b6095277428
Rahul Bethi <rahulbethi@hotmail.com>
2017-11-20 17:33:15 -0800

    Milestone: Build
    Added Failed Menu after 3 wrong matches
    Changed balls materials instead of colors
    Added Comments to the code
    Build 3

--- Project 2017.2/Assets/Scripts/GameLogic.cs
+++ Project 2017.2/Assets/Scripts/GameLogic.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-///using UnityEditor;
 
 public class GameLogic : MonoBehaviour
 {
 	public  GameObject   go_player;
-	public  GameObject   go_startUI, go_finishUI;
+	public  GameObject   go_startUI, go_gameWonUI, go_gameLostUI;
 	public  GameObject   go_startPoint, go_playPoint, go_finishPoint;
 	public  GameObject[] go_arr_light;
 	public  GameObject[] go_arr_puzzleSphere;
@@ -14,45 +13,55 @@ public class GameLogic : MonoBehaviour
 	public  AudioClip    winAudioClip;
 	private int[] arr_generatedSequence;
 	private int[] arr_inputSequence;
+
+	/// Random sequence animation speed
 	public  float seqAnimSpeed;
 	private int   flashLightsCounter;
-
 	private int  currentIndex;
+	public  int  totalChances;
+	private int  chancesFinished;
 	public  bool bool_takeInput = false;
 
 	//   S T A R T   //                                                                                                
 	void Start()
 	{
-		/// Update 'player' to be the camera's parent gameobject, i.e. GvrEditorEmulator, instead of the camera itself.
-		/// Required because GVR resets camera position to 0, 0, 0.
-		/// player = player.transform.parent.gameObject;
-
+		/// Declaring Arrays for random Sequences
 		arr_generatedSequence = new int[go_arr_puzzleSphere.Length];
 		arr_inputSequence     = new int[go_arr_puzzleSphere.Length];
 
-		/// Move player to the start position. 
+		/// Move player to the start position
 		go_player.transform.position = go_startPoint.transform.position;
 	}
 	
 	//   B U T T O N S   //
 	public void fn_startGame()
 	{
+		/// Disable Start menu
 		go_startUI.SetActive(false);
+
+		/// Start Game
 		fn_initGame();
 	}
 
 	public void fn_restartGame()
 	{
-		go_finishUI.SetActive(false);
+		/// Disable Restart Menu
+		go_gameWonUI.SetActive(false);
+		go_gameLostUI.SetActive(false);
+
+		/// Start Game
 		fn_initGame();
 	}
 	
 	//   I N I T   //
 	private void fn_initGame()
 	{
-		Debug.Log("Initiating Game");
-		/// Move Player to the Play Area.
+		/*Debug.Log("Initiating Game");*/
+
+		/// Boolean for disabling input while displaying the random sequence
 		bool_takeInput = false;
+
+		/// Move Player to the Play Area
 		fn_movePlayerToPoint(go_playPoint.transform.position);
 
 		/// Generating Random Sequence
@@ -60,24 +69,29 @@ public class GameLogic : MonoBehaviour
 		{
 			arr_generatedSequence[i] = Random.Range(0, arr_generatedSequence.Length);
 		}
-		Debug.Log ("The puzzle sequence is " + arr_generatedSequence[0] + ", " + arr_generatedSequence[1] + ", "
+		/* Debug.Log ("The puzzle sequence is " + arr_generatedSequence[0] + ", " + arr_generatedSequence[1] + ", "
 											 + arr_generatedSequence[2] + ", " + arr_generatedSequence[3] + ", "
-                                             + arr_generatedSequence[4]);
+                                             + arr_generatedSequence[4]);*/
+        /// Resetting number of failures. Game Lost of more than 3
+        chancesFinished = 0;
 
         /// Flash lights - for player attention
         fn_setLightsActive(true);
         CancelInvoke("fn_flashLights");
         flashLightsCounter = 0;
+        /// Call to turn lights on or off atlernatively - repeatedly
 		InvokeRepeating("fn_flashLights", 2.0f, 0.29f);
 
-        /// Animate Sequence
+        /// Animate Random Sequence
 		CancelInvoke("fn_animSequence");
 		currentIndex = 0;
 		fn_dimmBalls();
+		/// Call to show animation - one after another ball - repeatedly
 		InvokeRepeating("fn_animSequence", 4.5f, seqAnimSpeed);
 	}
 
 	//   L I G H T S   //
+	/// Disabling or enabling all spot lights at once
 	private void fn_setLightsActive(bool bool_status)
 	{
 		for(int i = 0; i < go_arr_light.Length; i++)
@@ -86,10 +100,12 @@ public class GameLogic : MonoBehaviour
 		}
 	}
 
+	/// Flashing spot lights
 	private void fn_flashLights()
 	{
 		fn_setLightsActive(!go_arr_light[0].activeSelf);
 		flashLightsCounter++;
+		/// Stop flashing lights after 3 flashes (6 for on and off both)
 		if(flashLightsCounter >= 6)
 		{
 			CancelInvoke("fn_flashLights");
@@ -97,6 +113,7 @@ public class GameLogic : MonoBehaviour
 	}
 
 	//   B A L L S   //
+	/// Stop glowing all balls at once
 	private void fn_dimmBalls()
 	{
 		for(int i = 0; i < go_arr_puzzleSphere.Length; i++)
@@ -105,6 +122,7 @@ public class GameLogic : MonoBehaviour
 		}
 	}
 
+	/// Glow all balls once
 	private void fn_glowBallsOnce()
 	{
 		for(int i = 0; i < go_arr_puzzleSphere.Length; i++)
@@ -113,103 +131,98 @@ public class GameLogic : MonoBehaviour
 		}
 	}
 
+	//   G A M E   L O G I C   //
+	/// Animate Random Sequence 
 	private void fn_animSequence()
 	{
+		/// Alternatively glow the ball in random sequence
 		if(currentIndex % 2 == 0)
 		{
 			go_arr_puzzleSphere[arr_generatedSequence[currentIndex / 2]].GetComponent<puzzleSphere>().fn_glow(true);
-			///go_arr_puzzleSphere[arr_generatedSequence[currentIndex / 2]].GetComponent<puzzleSphere>().fn_playAudio();
 		}
+		/// Stop glowing the last ball
 		else
 			go_arr_puzzleSphere[arr_generatedSequence[currentIndex / 2]].GetComponent<puzzleSphere>().fn_dimm();
 
+		/// counting number of times this function needs to be called
 		currentIndex++;
+		/// if counter exceeds double the number of balls (both on and off for glowing)
 		if(currentIndex >= (go_arr_puzzleSphere.Length * 2))
 		{
+			/// Stop Animating Sequence
 			CancelInvoke("fn_animSequence");
+			/// Call next step (flashing all balls once) after animation finishes
 			Invoke("fn_startTakingInput", 0.66f);
 		}
 	}
 
+	/// Glow all balls once to show that the random sequence animation is finished
 	private void fn_startTakingInput()
 	{
 		fn_glowBallsOnce();
+
+		/// Call next step (taking input) after flasing all lights
 		Invoke("fn_acceptInput", 0.33f);
 	}
 
+	/// Accept input
 	private void fn_acceptInput()
 	{
-		fn_glowBallsOnce();
 		currentIndex = 0;
+		/// start taking input
 		bool_takeInput = true;
-		Debug.Log("Accepting Input");
+		/*Debug.Log("Accepting Input");*/
 	}
 
+	/// This function is called when a ball is pressed - ball id is sent
 	public void fn_registerPoint(int p_id)
 	{
+		/// store the ball id which is clicked
 		arr_inputSequence[currentIndex] = p_id;
+		/// count for number of inputs
 		currentIndex++;
 
+		/// if number of inputs are reached, then stop taking input and check if the input is matched
 		if(currentIndex >= go_arr_puzzleSphere.Length)
 		{
 			/// Input sequence complete
 			bool_takeInput = false;
-			Debug.Log("Input complete: Stopped taking Input");
+			/*Debug.Log("Input complete: Stopped taking Input");*/
+			/// Check input sequence
 			fn_checkInput();
 		}
 	}
 
+	/// Matching Input Sequence
 	private void fn_checkInput()
 	{
 		for(int i = 0; i < go_arr_puzzleSphere.Length; i++)
 		{
+			/// If input is not matched in order, then wrong input
 			if(arr_generatedSequence[i] != arr_inputSequence[i])
 			{
+				/// Play fail audio
 				audioSource.clip = failAudioClip;
 				audioSource.Play();
-				Debug.Log("Input wrong");
-				fn_startTakingInput();
+				/*Debug.Log("Input wrong");*/
+				/// One chance finished
+				chancesFinished++;
+				/// If all chances are finished, then game lost
+				if(chancesFinished >= totalChances)
+					fn_gameOver(false);
+				/// If there are more chances, start taking input
+				else
+					fn_startTakingInput();
 				return;
 			}
 		}
-		Debug.Log("Input Correct");
-		fn_gameWon();
+		/*Debug.Log("Input Correct");*/
+		/// If now rong matches, then input is correct, Game Won
+		fn_gameOver(true);
 	}
-
-	/*public void fn_showSequence()
-	{
-		if(isBallGlowing == false)
-		{
-			fn_glowBall(arr_generatedSequence[currentIndex]);
-		}
-		else
-		{
-			fn_dimmAllSpheres();
-			currentIndex++;
-			if(currentIndex >= numOfSpheres)
-			{
-				CancelInvoke("fn_showSequence");
-			}
-		}
-
-		isBallGlowing = !isBallGlowing;
-
-	}*/
-
-	/*private void fn_glowBall(int ballIndex)
-	{
-		/// go_arr_puzzleSphere[ballindex].fn_glow(true);
-	}*/
-
-	/*private void fn_dimmAllSpheres()
-	{
-		for (int i = 0; i < numOfSpheres; i++)
-		{
-			/// go_arr_puzzleSphere[i].fn_glow(false);
-		}
-	}*/
 	
 	//   P L A Y E R   M O V E M E N T   //
+	/// iTween movement to a point
 	private void fn_movePlayerToPoint(Vector3 point)
 	{
 		iTween.MoveTo
@@ -223,35 +236,36 @@ public class GameLogic : MonoBehaviour
 			)
 		);
 	}
-
-	/*//   U P D A T E   //                                                                                              
-	void Update()
-	{
-		
-		/*if(Input.GetMouseButtonDown(0) && go_player.transform.position == go_playPoint.transform.position)
-		{
-			fn_gameWon();
-		}*/
-
-		/*if(go_player.transform.position == go_playPoint.transform.position && bool_takeInput == true)
-		{
-			Invoke("fn_gameWon", 25.0f);
-		}
-	}*/
 	
-	//   W I N   //
-	private void fn_gameWon()
+	//   G A M E   O V E R   //
+	/// Game Over function
+	private void fn_gameOver(bool bool_status)
 	{
-		audioSource.clip = winAudioClip;
-		audioSource.Play();
-		Debug.Log("Game Won");
+		/// Exit the room
 		fn_movePlayerToPoint(go_finishPoint.transform.position);
-		go_finishUI.SetActive (true);
-		CancelInvoke("fn_gameWon");
+
+		/// true is sent for game won
+		if(bool_status)
+		{
+			/// Game won audio
+			audioSource.clip = winAudioClip;
+			audioSource.Play();
+			/*Debug.Log("Game Won");*/
+
+			/// Show game Won Restart Menu
+			go_gameWonUI.SetActive(true);
+		}
+		/// False is recieved if game is lost
+		else
+		{
+			/// Game Lost Restart Menu
+			go_gameLostUI.SetActive(true);
+		}
 	}
 
 	void Update()
 	{
+		/// To quit the application when X button is pressed
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
         	Application.Quit();
--- Project 2017.2/Assets/Scripts/puzzleSphere.cs
+++ Project 2017.2/Assets/Scripts/puzzleSphere.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
-///using System.Collections.Generic;
 using UnityEngine;
 
 public class puzzleSphere : MonoBehaviour
 {
 	private Renderer rend;
-	public  Color origColor;
-	public  Color glowColor;
+	public  Material mat_normal;
+	public  Material mat_glow;
 	public  int   id;
 	public  AudioSource audioSource;
 
@@ -14,66 +13,66 @@ public class puzzleSphere : MonoBehaviour
 
 	void Start()
 	{
+		/// Get teh renderer of the ball
 		rend = GetComponent<Renderer>();
 	}
 
+	/// Function to glow the ball. True is sent to play audio along with it
 	public void fn_glow(bool bool_status)
 	{
-		rend.material.SetColor("_Color", glowColor);
+		/// Change material to glow
+		rend.material = mat_glow;
+		/// Play ball hit audio if true
 		if(bool_status)
 			audioSource.Play();
 	}
 
+	/// Function to reset the material to normal
 	public void fn_dimm()
 	{
-		rend.material.SetColor("_Color", origColor);
+		rend.material = mat_normal;
 	}
 
+	/// This function glows the ball for 0.33 sec
 	public void fn_glowOnce()
 	{
 		fn_glow(false);
+		/// Reset back to normal after 0.33 sec
 		Invoke("fn_dimm",0.33f);
 	}
 
-	/*public void fn_buttonPressed()
-	{
-		if(go_gameLogic.GetComponent<GameLogic>().bool_takeInput == true)
-		{
-			fn_glow();
-			Debug.Log(this.gameObject.name + ": pressed");
-		}
-	}*/
-
+	/// When ball is clicked
 	public void fn_clicked()
 	{
+		/// Accept input only when the game Logic sets te flag to accept input
 		if(go_gameLogic.GetComponent<GameLogic>().bool_takeInput == true)
 		{
-			Debug.Log(this.gameObject.name + ": clicked");
+			/*Debug.Log(this.gameObject.name + ": clicked");*/
+			/// Call the Register point function in gameLogic
 			go_gameLogic.GetComponent<GameLogic>().fn_registerPoint(this.id);
 			audioSource.Play();
 		}
 	}
 
-	public void fn_playAudio()
-	{
-		audioSource.Play();
-	}
-
+	/// When player points at the ball
 	public void fn_playerStartedLooking()
 	{
+		/// Glow only when thje gameLogic accepts input
 		if(go_gameLogic.GetComponent<GameLogic>().bool_takeInput == true)
 		{
 			fn_glow(false);
-			Debug.Log(this.gameObject.name + ": Player started looking at me");
+			/*Debug.Log(this.gameObject.name + ": Player started looking at me");*/
 		}
 	}
 
+	/// When player stops looking at the ball
 	public void fn_playerStoppedLooking()
 	{
+		/// Back to normal
 		if(go_gameLogic.GetComponent<GameLogic>().bool_takeInput == true)
 		{
 			fn_dimm();
-			Debug.Log(this.gameObject.name + ": Player stopped looking at me");
+			/*Debug.Log(this.gameObject.name + ": Player stopped looking at me");*/
 		}
 	}
 }
